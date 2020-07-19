4d3f077b83cd1aa2e195b9607928d2bd1b2541ca
Meldexun <27779321+Meldexun@users.noreply.github.com>
2020-07-19 02:37:12 +0200

    Removed unnecessary syncing

--- src/main/java/meldexun/better_diving/network/handler/CPacketHandlerSyncOxygen.java
+++ src/main/java/meldexun/better_diving/network/handler/CPacketHandlerSyncOxygen.java
@@ -3,11 +3,8 @@ package meldexun.better_diving.network.handler;
 import meldexun.better_diving.BetterDiving;
 import meldexun.better_diving.capability.diving.CapabilityDivingAttributesProvider;
 import meldexun.better_diving.capability.diving.ICapabilityDivingAttributes;
-import meldexun.better_diving.capability.item.oxygen.CapabilityOxygenProvider;
-import meldexun.better_diving.capability.item.oxygen.ICapabilityOxygen;
 import meldexun.better_diving.network.packet.SPacketSyncOxygen;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
@@ -23,11 +20,6 @@ public class CPacketHandlerSyncOxygen implements IMessageHandler<SPacketSyncOxyg
 				ICapabilityDivingAttributes idiving = player.getCapability(CapabilityDivingAttributesProvider.DIVING_ATTRIBUTES, null);
 
 				idiving.setOxygen(message.getOxygenPlayer());
-
-				ICapabilityOxygen ioxygen = player.getItemStackFromSlot(EntityEquipmentSlot.CHEST).getCapability(CapabilityOxygenProvider.OXYGEN, null);
-				if (ioxygen != null) {
-					ioxygen.setOxygen(message.getOxygenTank());
-				}
 			}
 		});
 		return null;
