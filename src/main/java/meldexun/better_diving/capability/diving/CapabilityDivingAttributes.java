4fcbd40ca997d4932422c063c1fb5e15a3fc8bd4
Meldexun <27779321+Meldexun@users.noreply.github.com>
2020-07-19 02:37:47 +0200

    Fixed movement desync which could cause oxygen desync

--- src/main/java/meldexun/better_diving/capability/diving/CapabilityDivingAttributes.java
+++ src/main/java/meldexun/better_diving/capability/diving/CapabilityDivingAttributes.java
@@ -23,6 +23,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Enchantments;
 import net.minecraft.init.MobEffects;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumParticleTypes;
@@ -139,9 +140,9 @@ public class CapabilityDivingAttributes implements ICapabilityDivingAttributes {
 		this.isPrevInSeamoth = this.isInSeamoth;
 		this.isInSeamoth = this.player.getRidingEntity() instanceof EntitySeamoth;
 
+		this.handleMovement();
 		if (this.player.world.isRemote) {
 			this.handleDiving();
-			this.handleMovement();
 			if (!BetterDivingConfig.getInstance().general.oxygenSyncPackets) {
 				this.handleOxygen();
 			}
@@ -186,88 +187,55 @@ public class CapabilityDivingAttributes implements ICapabilityDivingAttributes {
 
 	protected void handleMovement() {
 		if (BetterDivingConfig.getInstance().modules.divingMovement && this.player.isInWater() && !this.player.capabilities.isFlying && !this.player.isRiding()) {
-			GameSettings settings = Minecraft.getMinecraft().gameSettings;
-
-			boolean inputForward = settings.keyBindForward.isKeyDown();
-			boolean inputBack = settings.keyBindBack.isKeyDown();
-			boolean inputRight = settings.keyBindRight.isKeyDown();
-			boolean inputLeft = settings.keyBindLeft.isKeyDown();
-			boolean inputUp = settings.keyBindJump.isKeyDown();
-			boolean inputDown = settings.keyBindSneak.isKeyDown();
-
-			float rotationPitch = this.player.rotationPitch;
-			float rotationYaw = this.player.rotationYaw;
-
-			double slow = this.calculateSlow();
-			double speed = this.getSwimSpeedFromPlayer();
-
-			double strafe = 0.0D;
-			double forward = 0.0D;
-			double up = 0.0D;
-			if (inputForward) {
-				forward++;
-			}
-			if (inputBack) {
-				forward--;
-			}
-			if (inputRight) {
-				strafe--;
-			}
-			if (inputLeft) {
-				strafe++;
-			}
-			if (inputUp) {
-				up++;
-			}
-			if (inputDown) {
-				up--;
-			}
-
-			if (!BetterDivingConfig.getInstance().general.vanillaDivingMovement) {
-				this.player.motionY += 0.02D;
-
-				if (inputDown) {
-					slow *= 0.3D;
-				}
-
-				if (inputUp) {
-					this.player.motionY -= 0.03999999910593033D;
-				}
-
-				if (inputForward != inputBack || inputRight != inputLeft) {
-					MovementHelper.move2D(this.player, strafe, forward, -slow, rotationYaw);
-				}
-
-				if (inputForward != inputBack || inputRight != inputLeft || inputUp != inputDown) {
-					if (inputForward && !inputBack) {
-						if (inputUp && !inputDown) {
-							rotationPitch = (rotationPitch - 90.0F) / 2.0F;
-							up = 0;
-						} else if (inputDown && !inputUp) {
-							rotationPitch = (rotationPitch + 90.0F) / 2.0F;
-							up = 0;
-						}
-					} else if (inputBack && !inputForward) {
-						if (inputUp && !inputDown) {
-							rotationPitch = (rotationPitch + 90.0F) / 2.0F;
-							up = 0;
-						} else if (inputDown && !inputUp) {
-							rotationPitch = (rotationPitch - 90.0F) / 2.0F;
-							up = 0;
-						}
+			if (!this.player.world.isRemote) {
+				if (!BetterDivingConfig.getInstance().general.vanillaDivingMovement) {
+					this.player.motionY += 0.02D;
+				} else {
+					this.player.motionY += 0.015D;
+					if (this.isDiving) {
+						this.player.motionY += 0.005D;
 					}
-
-					if (ItemSeaglide.canUseSeaglide(this.player) && !inputForward) {
-						speed *= 0.4D;
-					}
-
-					this.move3D(this.player, strafe, up, forward, speed, rotationYaw, rotationPitch);
 				}
 			} else {
-				this.player.motionY += 0.015D;
+				GameSettings settings = Minecraft.getMinecraft().gameSettings;
 
-				if ((settings.keyBindSprint.isKeyDown() || this.player.isSprinting()) && inputForward && !inputBack) {
-					this.player.motionY += 0.005D;
+				boolean inputForward = settings.keyBindForward.isKeyDown();
+				boolean inputBack = settings.keyBindBack.isKeyDown();
+				boolean inputRight = settings.keyBindRight.isKeyDown();
+				boolean inputLeft = settings.keyBindLeft.isKeyDown();
+				boolean inputUp = settings.keyBindJump.isKeyDown();
+				boolean inputDown = settings.keyBindSneak.isKeyDown();
+
+				float rotationPitch = this.player.rotationPitch;
+				float rotationYaw = this.player.rotationYaw;
+
+				double slow = this.calculateSlow();
+				double speed = this.getSwimSpeedFromPlayer();
+
+				double strafe = 0.0D;
+				double forward = 0.0D;
+				double up = 0.0D;
+				if (inputForward) {
+					forward++;
+				}
+				if (inputBack) {
+					forward--;
+				}
+				if (inputRight) {
+					strafe--;
+				}
+				if (inputLeft) {
+					strafe++;
+				}
+				if (inputUp) {
+					up++;
+				}
+				if (inputDown) {
+					up--;
+				}
+
+				if (!BetterDivingConfig.getInstance().general.vanillaDivingMovement) {
+					this.player.motionY += 0.02D;
 
 					if (inputDown) {
 						slow *= 0.3D;
@@ -277,22 +245,66 @@ public class CapabilityDivingAttributes implements ICapabilityDivingAttributes {
 						this.player.motionY -= 0.03999999910593033D;
 					}
 
-					MovementHelper.move2D(this.player, strafe, forward, -slow, rotationYaw);
-
-					if (inputUp && !inputDown) {
-						rotationPitch = (rotationPitch - 90.0F) / 2.0F;
-						up = 0;
-					} else if (inputDown && !inputUp) {
-						rotationPitch = (rotationPitch + 90.0F) / 2.0F;
-						up = 0;
+					if (inputForward != inputBack || inputRight != inputLeft) {
+						MovementHelper.move2D(this.player, strafe, forward, -slow, rotationYaw);
 					}
 
-					this.move3D(this.player, strafe, up, forward, speed, rotationYaw, rotationPitch);
-				} else if (inputDown) {
-					this.player.motionY -= 0.03999999910593033D;
-					slow *= 0.7D;
+					if (inputForward != inputBack || inputRight != inputLeft || inputUp != inputDown) {
+						if (inputForward && !inputBack) {
+							if (inputUp && !inputDown) {
+								rotationPitch = (rotationPitch - 90.0F) / 2.0F;
+								up = 0;
+							} else if (inputDown && !inputUp) {
+								rotationPitch = (rotationPitch + 90.0F) / 2.0F;
+								up = 0;
+							}
+						} else if (inputBack && !inputForward) {
+							if (inputUp && !inputDown) {
+								rotationPitch = (rotationPitch + 90.0F) / 2.0F;
+								up = 0;
+							} else if (inputDown && !inputUp) {
+								rotationPitch = (rotationPitch - 90.0F) / 2.0F;
+								up = 0;
+							}
+						}
 
-					MovementHelper.move2D(this.player, strafe, forward, slow, rotationYaw);
+						if (ItemSeaglide.canUseSeaglide(this.player) && !inputForward) {
+							speed *= 0.4D;
+						}
+
+						this.move3D(this.player, strafe, up, forward, speed, rotationYaw, rotationPitch);
+					}
+				} else {
+					this.player.motionY += 0.015D;
+
+					if ((settings.keyBindSprint.isKeyDown() || this.player.isSprinting()) && inputForward && !inputBack) {
+						this.player.motionY += 0.005D;
+
+						if (inputDown) {
+							slow *= 0.3D;
+						}
+
+						if (inputUp) {
+							this.player.motionY -= 0.03999999910593033D;
+						}
+
+						MovementHelper.move2D(this.player, strafe, forward, -slow, rotationYaw);
+
+						if (inputUp && !inputDown) {
+							rotationPitch = (rotationPitch - 90.0F) / 2.0F;
+							up = 0;
+						} else if (inputDown && !inputUp) {
+							rotationPitch = (rotationPitch + 90.0F) / 2.0F;
+							up = 0;
+						}
+
+						this.move3D(this.player, strafe, up, forward, speed, rotationYaw, rotationPitch);
+					} else if (inputDown) {
+						this.player.motionY -= 0.03999999910593033D;
+						slow *= 0.7D;
+
+						MovementHelper.move2D(this.player, strafe, forward, slow, rotationYaw);
+					}
 				}
 			}
 		}
@@ -335,16 +347,13 @@ public class CapabilityDivingAttributes implements ICapabilityDivingAttributes {
 			double d4 = Math.cos(pitch * 0.017453292D);
 
 			double depthStriderFactor = 1.0D;
-			ItemStack feet = player.inventory.armorInventory.get(0);
-			double depthStrider = EnchantmentHelper.getEnchantmentLevel(Enchantments.DEPTH_STRIDER, feet);
-			if (depthStrider > 0.0D) {
-				if (depthStrider > 3.0D) {
-					depthStrider = 3.0D;
+			ItemStack feet = player.getItemStackFromSlot(EntityEquipmentSlot.FEET);
+			int depthStriderLevel = EnchantmentHelper.getEnchantmentLevel(Enchantments.DEPTH_STRIDER, feet);
+			if (depthStriderLevel > 0) {
+				if (depthStriderLevel > 3) {
+					depthStriderLevel = 3;
 				}
-				if (!player.onGround) {
-					depthStrider *= 0.5D;
-				}
-				depthStriderFactor += 1.2699997 / 3.0D * depthStrider;
+				depthStriderFactor += 1.2699997D / 3.0D * depthStriderLevel * (!player.onGround ? 0.5D : 1.0D);
 			}
 
 			player.motionX += (strafe * d2 - forward * d1 * d4) * depthStriderFactor;
--- src/main/java/meldexun/better_diving/util/MovementHelper.java
+++ src/main/java/meldexun/better_diving/util/MovementHelper.java
@@ -1,6 +1,11 @@
 package meldexun.better_diving.util;
 
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Enchantments;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
 
 public class MovementHelper {
 
@@ -45,6 +50,40 @@ public class MovementHelper {
 			entity.motionX += strafe * d2 - forward * d1 * d4;
 			entity.motionY += up - forward * d3;
 			entity.motionZ += forward * d2 * d4 + strafe * d1;
+		}
+	}
+
+	public static void move3DRespectDepthStrider(EntityLivingBase entity, double strafe, double up, double forward, double speed, double yaw, double pitch) {
+		double d = strafe * strafe + up * up + forward * forward;
+		if (d >= 1.0E-4D) {
+			d = Math.sqrt(d);
+			if (d < 1.0D) {
+				d = 1.0D;
+			}
+			d = speed / d;
+
+			strafe *= d;
+			up *= d;
+			forward *= d;
+
+			double d1 = Math.sin(yaw * 0.017453292D);
+			double d2 = Math.cos(yaw * 0.017453292D);
+			double d3 = Math.sin(pitch * 0.017453292D);
+			double d4 = Math.cos(pitch * 0.017453292D);
+
+			double depthStriderFactor = 1.0D;
+			ItemStack feet = entity.getItemStackFromSlot(EntityEquipmentSlot.FEET);
+			int depthStriderLevel = EnchantmentHelper.getEnchantmentLevel(Enchantments.DEPTH_STRIDER, feet);
+			if (depthStriderLevel > 0) {
+				if (depthStriderLevel > 3) {
+					depthStriderLevel = 3;
+				}
+				depthStriderFactor += 1.2699997D / 3.0D * depthStriderLevel * (!entity.onGround ? 0.5D : 1.0D);
+			}
+
+			entity.motionX += (strafe * d2 - forward * d1 * d4) * depthStriderFactor;
+			entity.motionY += up - forward * d3;
+			entity.motionZ += (forward * d2 * d4 + strafe * d1) * depthStriderFactor;
 		}
 	}
 
