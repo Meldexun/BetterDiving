cb5ff5f43a8fe5e528c08bf77f488b1299b3d300
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-01-28 03:49:26 +0100

    Cleanup

--- src/main/java/meldexun/better_diving/item/ItemPowerCell.java
+++
@@ -1,9 +0,0 @@
-package meldexun.better_diving.item;
-
-public class ItemPowerCell extends ItemEnergyStorage {
-
-	public ItemPowerCell(int capacity, int maxReceive, int maxExtract) {
-		super(capacity, maxReceive, maxExtract);
-	}
-
-}
