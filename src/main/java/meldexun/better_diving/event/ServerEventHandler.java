78f5b4aba8779cf0e8a060321e39a1d8337e543a
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-05-17 14:49:46 +0200

    Revert "Disable damage from magma blocks with water above"
    This reverts commit 69f799cd83186079c9329028b1004dca79359226.

--- src/main/java/meldexun/better_diving/event/ServerEventHandler.java
+++ src/main/java/meldexun/better_diving/event/ServerEventHandler.java
@@ -4,15 +4,8 @@ import meldexun.better_diving.BetterDiving;
 import meldexun.better_diving.api.event.PlayerCanBreathEvent;
 import meldexun.better_diving.entity.EntitySeamoth;
 import meldexun.better_diving.oxygenprovider.DivingGearManager;
-import net.minecraft.block.MagmaBlock;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.tags.FluidTags;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import net.minecraftforge.event.entity.EntityEvent;
-import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -37,18 +30,6 @@ public class ServerEventHandler {
 	public static void onPlayerWaterBreathingEvent(PlayerCanBreathEvent event) {
 		if (event.getPlayer().getRidingEntity() instanceof EntitySeamoth && ((EntitySeamoth) event.getPlayer().getRidingEntity()).hasEnergy()) {
 			event.setCanBreath(true);
-		}
-	}
-
-	@SubscribeEvent
-	public static void onLivingAttackEvent(LivingAttackEvent event) {
-		if (event.getSource() == DamageSource.HOT_FLOOR) {
-			Entity entity = event.getEntity();
-			BlockPos pos = new BlockPos(entity.getPosX(), entity.getPosY() - 1.0E-4D, entity.getPosZ());
-			World world = entity.world;
-			if (world.getBlockState(pos).getBlock() instanceof MagmaBlock && world.getFluidState(pos.up()).getFluid().isIn(FluidTags.WATER)) {
-				event.setCanceled(true);
-			}
 		}
 	}
 
