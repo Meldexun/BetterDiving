0423d4e3523c57187f96bac120ace617761cc88c
Meldexun <27779321+Meldexun@users.noreply.github.com>
2021-06-13 02:40:08 +0200

    Remove unused methods

--- src/main/java/meldexun/better_diving/util/OxygenItemHelper.java
+++ src/main/java/meldexun/better_diving/util/OxygenItemHelper.java
@@ -10,54 +10,6 @@ import net.minecraftforge.common.util.LazyOptional;
 public class OxygenItemHelper {
 
 	/**
-	 * Returns the amount of oxygen this item currently holds.
-	 */
-	public static int getOxygen(ItemStack stack) {
-		LazyOptional<ICapabilityOxygenItem> optionalOxygenCap = stack.getCapability(CapabilityOxygenItemProvider.OXYGEN, null);
-		if (!optionalOxygenCap.isPresent()) {
-			return 0;
-		}
-		ICapabilityOxygenItem oxygenCap = optionalOxygenCap.orElseThrow(NullPointerException::new);
-		return oxygenCap.getOxygen();
-	}
-
-	/**
-	 * Returns the amount of oxygen this item can hold.
-	 */
-	public static int getOxygenCapacity(ItemStack stack) {
-		LazyOptional<ICapabilityOxygenItem> optionalOxygenCap = stack.getCapability(CapabilityOxygenItemProvider.OXYGEN, null);
-		if (!optionalOxygenCap.isPresent()) {
-			return 0;
-		}
-		ICapabilityOxygenItem oxygenCap = optionalOxygenCap.orElseThrow(NullPointerException::new);
-		return oxygenCap.getOxygenCapacity();
-	}
-
-	/**
-	 * Returns the amount of oxygen that was received.
-	 */
-	public static int receiveOxygen(ItemStack stack, int amount) {
-		LazyOptional<ICapabilityOxygenItem> optionalOxygenCap = stack.getCapability(CapabilityOxygenItemProvider.OXYGEN, null);
-		if (!optionalOxygenCap.isPresent()) {
-			return 0;
-		}
-		ICapabilityOxygenItem oxygenCap = optionalOxygenCap.orElseThrow(NullPointerException::new);
-		return oxygenCap.receiveOxygen(amount);
-	}
-
-	/**
-	 * Returns the amount of oxygen that was extracted.
-	 */
-	public static int extractOxygen(ItemStack stack, int amount) {
-		LazyOptional<ICapabilityOxygenItem> optionalOxygenCap = stack.getCapability(CapabilityOxygenItemProvider.OXYGEN, null);
-		if (!optionalOxygenCap.isPresent()) {
-			return 0;
-		}
-		ICapabilityOxygenItem oxygenCap = optionalOxygenCap.orElseThrow(NullPointerException::new);
-		return oxygenCap.extractOxygen(amount);
-	}
-
-	/**
 	 * This should return 0 when this item is not in a valid slot!<br>
 	 * Returns the amount of oxygen this item currently holds.
 	 */
